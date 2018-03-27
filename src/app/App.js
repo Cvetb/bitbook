3960f111de6cb5b3d96418f6cce4357423c12908
BIT Student <classroom@bgit.rs>
2018-03-27 16:30:18 +0200

    created footer

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Footer from "./partials/Footer.js"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-     
-      </div>
+      <Footer/>
     );
   }
 }
 
 export default App;
+
+

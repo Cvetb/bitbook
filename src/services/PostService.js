c8f90d53d78995bfedc99089e6ab27fbb655db64
BIT Student <classroom@bgit.rs>
2018-03-28 17:29:12 +0200

    new-post-service

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -8,9 +8,9 @@ class PostService {
     return fetch("http://bitbookapi.azurewebsites.net/api/Posts", {
       method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'Key': 'bitbook',
-        'SessionId': '7A5D8FF8-B04D-4C8C-9812-8B44EB7E4C94'
+        "Content-Type": "application/json",
+        "Key": "bitbook",
+        "SessionId": "7A5D8FF8-B04D-4C8C-9812-8B44EB7E4C94"
       }
     })
       .then(response => {
@@ -18,19 +18,64 @@ class PostService {
       })
       .then(result => {
         return result.map(post => {
-
           if (post.type === "text") {
             return new TextPost(post);
-
           } else if (post.type === "image") {
             return new ImagePost(post);
-
-          } else if (post.type === "video"){
+          } else if (post.type === "video") {
             return new VideoPost(post);
           }
         });
       });
   }
+  newTextPost(post) {
+    return fetch("http://bitbookapi.azurewebsites.net/api/TextPosts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Key": "bitbook",
+        "SessionId": "7A5D8FF8-B04D-4C8C-9812-8B44EB7E4C94"
+      },
+      body: JSON.stringify({
+        text: post.text,
+        postId: post.id
+      })
+    }).then(response => response.json());
+    
+  }
+
+  newImagePost(post) {
+    return fetch("http://bitbookapi.azurewebsites.net/api/ImagePosts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Key": "bitbook",
+        "SessionId": "7A5D8FF8-B04D-4C8C-9812-8B44EB7E4C94"
+      },
+      body: JSON.stringify({
+        imageUrl: post.imageUrl,
+        postId: post.id
+      })
+    }).then(response => response.json());
+    
+     
+  }
+  newVideoPost(post) {
+    return fetch("http://bitbookapi.azurewebsites.net/api/VideoPosts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Key": "bitbook",
+        "SessionId": "7A5D8FF8-B04D-4C8C-9812-8B44EB7E4C94"
+      },
+      body: JSON.stringify({
+        videoUrl: post.videoUrl,
+        postId: post.id
+      })
+    }).then(response => response.json());
+    
+  }
 }
+
 
 export const postService = new PostService();
